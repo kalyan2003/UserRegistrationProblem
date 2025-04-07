aa62e064d85efb2135ca94203e9a3d2cfea03298
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-04-07 15:51:36 +0530

    Implemented the use case 1 , which validtes the first name based on conditions

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -1,2 +1,20 @@
-print("Hello world")
+import re
+
+First_Name = input("Enter your first name :")
+
+
+def validate_first_and_last_name(First_Name):
+    """
+    Function to validate the first and last name
+    :param First_Name:
+    :return: bool value based on the condition matches
+    """
+    pattern = r"^[A-Z][a-z]{2,}$"
+    return bool(re.fullmatch(pattern, First_Name))
+
+
+print(validate_first_and_last_name(First_Name))
+
+
+
 
