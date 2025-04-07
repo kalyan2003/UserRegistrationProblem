3be383d20f44d8c914845d299c4de788bcf3db1b
kalyan2003 <pavan.chimmiri2003@gmail.com>
2025-04-07 17:06:21 +0530

    Implemented the usecase-4 which states to valid the given mobile number

--- user_registration_problem.py
+++ user_registration_problem.py
@@ -10,6 +10,32 @@ def validate_first_and_last_name(First_Name):
     return bool(re.fullmatch(pattern, First_Name))
 
 
+def validate_email_id(mail):
+    """
+    Function to check the given mail id is correct or false
+    :param mail:
+    :return: bool value based on the condition matches
+    """
+    pattern = r"^[a-z]+(\.[a-z0-9]+)?@[a-z]+\.[a-z]{2,3}(\.[a-z]{2})?$"
+
+    return bool(re.fullmatch(pattern, mail))
+
+
+def validate_mobile_number(mobile_number):
+    """
+    Validate the given mobile number is valid or not
+    :param mobile_number:
+    :return: bool value based on the condition matches
+    """
+    pattern = r"^91\s[6-9]\d{9}"
+
+    return bool(re.fullmatch(pattern, mobile_number))
+
+
+
+
+
+
 First_Name = input("Enter your first name :")
 
 validate_first_and_last_name(First_Name)
@@ -17,4 +43,19 @@ validate_first_and_last_name(First_Name)
 last_name = input("Enter your last name :")
 
 validate_first_and_last_name(last_name)
+
+mail_id = input("Enter your mail id: ")
+
+validate_email_id(mail_id)
+
+mobile_number = input("Enter your mobile number: ")
+validate_mobile_number(mobile_number)
+
+
+
+
+
+
+
+
 
